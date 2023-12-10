11c4dcb8dfd2303fff02f03a1077d313942c783f
Abdiqani <abdiqaniadan254@gmail.com>
2023-12-10 21:17:33 +0300

    combining

--- lib/convert_to_24_hour.py
+++ lib/convert_to_24_hour.py
@@ -10,3 +10,9 @@ def convert_to_24_hour(hour, minute, period):
     hour_str = str(hour).zfill(2)
     minute_str = str(minute).zfill(2)
 
+ # Combining hour and minutes str
+    time_24_hour = hour_str + minute_str
+    print( time_24_hour)
+
+
+convert_to_24_hour (12, 30, "am")
