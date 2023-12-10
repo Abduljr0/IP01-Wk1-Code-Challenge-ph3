871c1636a5a91acdd743a833730955c14025adf2
Abdiqani <abdiqaniadan254@gmail.com>
2023-12-10 21:15:27 +0300

    formating to 2 digits

--- lib/convert_to_24_hour.py
+++ lib/convert_to_24_hour.py
@@ -6,4 +6,7 @@ def convert_to_24_hour(hour, minute, period):
         if hour != 12:
             hour += 12
 
-   
+   # Formatting hour and minute to have two digits each
+    hour_str = str(hour).zfill(2)
+    minute_str = str(minute).zfill(2)
+
